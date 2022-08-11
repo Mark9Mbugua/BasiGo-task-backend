fee2874b22e2c8fb518c9a49c8e8e9c21b15ac9a
Mbugua <mbuguamark@adalabsafrica.com>
2022-08-11 10:00:22 +0300

    Feature: Create Customer model from Lead model

--- constants/functions/leads.js
+++ constants/functions/leads.js
@@ -4,12 +4,11 @@ const { uploadFile } = require("../../s3");
 
 /* Handle Customer Details */
 exports.HANDLE_CUSTOMER_DETAILS = async (leadId, photo, annualEarning) => {
-  console.log(photo);
   const result = await uploadFile(photo);
 
   await db.customers.create({
     leadId,
-    photo: result,
+    photo: result.Location,
     annualEarning,
   });
 };
--- s3.js
+++ s3.js
@@ -1,25 +1,21 @@
 require("dotenv").config();
 const fs = require("fs");
+const AWS = require("aws-sdk");
 const S3 = require("aws-sdk/clients/s3");
 
-// const bucketName = process.env.AWS_BUCKET_NAME;
-// const region = process.env.AWS_BUCKET_REGION;
-// const accessKeyId = process.env.AWS_ACCESS_KEY;
-// const secretAccesskey = process.env.AWS_SECRET_KEY;
-
-const bucketName = "basigo-challenge";
-const region = "ap-south-1";
-const accessKeyId = "AKIA3BJRUBRVE2C4HQXN";
-const secretAccesskey = "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";
+const bucketName = process.env.AWS_BUCKET_NAME;
+const region = process.env.AWS_BUCKET_REGION;
+const accessKeyId = process.env.AWS_ACCESS_KEY;
+const secretAccessKey = process.env.AWS_SECRET_KEY;
 
 const s3 = new S3({
-  region,
   accessKeyId,
-  secretAccesskey,
+  secretAccessKey,
+  region,
 });
 
 //upload a file to s3
-module.exports = function uploadFile(file) {
+function uploadFile(file) {
   const fileStream = fs.createReadStream(file.path);
 
   const uploadParams = {
@@ -27,7 +23,7 @@ module.exports = function uploadFile(file) {
     Body: fileStream,
     Key: file.filename,
   };
-  console.log(s3.upload(uploadParams).promise());
   return s3.upload(uploadParams).promise();
-};
-// exports.uploadFile = uploadFile;
+}
+
+exports.uploadFile = uploadFile;
